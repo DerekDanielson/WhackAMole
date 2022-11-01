a6522287d137f4c5bc2b7426c94ccaf52d41493c
Derek Danielson <derekdanielson84@gmail.com>
2022-11-01 10:58:48 -0600

    CSS and JS work

---
+++ app.js
@@ -0,0 +1,20 @@
+//MAKE SQUARES GREEN AND PINK AND ON EVERY CLICK CHANGE SQUARE COLOR
+
+const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const timeLeft = document.querySelector('#time-left')
+const score = document.querySelector('#score')
+
+let result = 0
+
+//Remove mole to start the game
+function randomSquare() {
+    squares.forEach(square => {
+        square.classList.remove('mole')
+    })
+
+    let randomSquare = squares[Math.floor(Math.random() * 9)]
+    randomSquare.classList.add('.mole')
+}
+
+randomSquare()
